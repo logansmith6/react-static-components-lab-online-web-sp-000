3b96b28b65252dd4a2ae16ff051d69548d136dec
Logan Smith <logsmith06@gmail.com>
2021-04-07 21:37:10 +0000

    Done.

--- src/GraceHopperQuoteComponent.js
+++ src/GraceHopperQuoteComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-export class GraceHopperQuoteComponent extends Component {
+export default class GraceHopperQuoteComponent extends Component {
 	render() {
 		return (
-			<div className="bar" id="hopper">
+			<div className="bar" id="hopper" >
 				<i>
 					<p>
 						&quot;The most dangerous phrase in the language is, ‘We’ve always done it this way.’&quot;
