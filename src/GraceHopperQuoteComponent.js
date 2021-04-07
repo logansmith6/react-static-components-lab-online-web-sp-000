8217dbad6a299f49bfd65bbcd74ccb26f1a434c9
Logan Smith <logsmith06@gmail.com>
2021-04-07 21:35:49 +0000

    Automatically backed up by Learn

--- src/GraceHopperQuoteComponent.js
+++ src/GraceHopperQuoteComponent.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class GraceHopperQuoteComponent extends Component {
 	render() {
 		return (
-			<div className="bar" id="hopper" >{// eslint-disable-next-line}
+			<div className="bar" id="hopper" >
 				<i>
 					<p>
 						&quot;The most dangerous phrase in the language is, ‘We’ve always done it this way.’&quot;
